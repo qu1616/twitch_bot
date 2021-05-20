49a119f9a84d2f18b9d9726ed08f1a0f064e7c87
qam6227 <qam6227@rit.edu>
2021-05-20 18:16:31 -0400

    finally got everything to work

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+from twitchio.ext import commands
+
+bot = commands.Bot(
+    irc_token=os.environ['TMI_TOKEN'], 
+    client_id=os.environ['CLIENT_ID'], 
+    nick=os.environ['BOT_NICK'], 
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+if __name__ == "__main__":
+    bot.run()
 
